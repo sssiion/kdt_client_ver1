d21b3797eb93bb34e0089985cc7e6f7c114dc662
sssiion <rta2752775@gmail.com>
2025-08-08 11:19:02 +0900

    Delete src/main directoryㅇㅇ

--- src/main/java/org/example/kdt_bank_client2/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example.kdt_bank_client2;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(ChatClientApp.class, args);
-    }
-}
